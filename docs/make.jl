856b02f61c26272734779ceaa6484aa78675a4c7
Phillip Alday <me@phillipalday.com>
2023-09-04 23:16:27 +0200

    boilerplate and initial dataset functionality

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+using Documenter
+using DocStringExtensions
+using MixedModelsDatasets
+
+makedocs(; root=joinpath(dirname(pathof(MixedModelsDatasets)), "..", "docs"),
+         sitename="BoxCox",
+         doctest=true,
+         strict=true,
+         pages=["index.md"])
+
+deploydocs(; repo="github.com/JuliaMixedModels/MixedModelsDatasets.jl", push_preview=true, devbranch="main")
