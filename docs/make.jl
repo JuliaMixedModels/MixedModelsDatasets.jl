874c27a35de57ef5f354173a65897d5d0672207e
Phillip Alday <me@phillipalday.com>
2026-05-23 04:43:26 +0000

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,13 @@ using Documenter
 using MixedModelsDatasets
 
 makedocs(;
-        #  format=Documenter.HTML(; size_threshold=500_000, size_threshold_warn=250_000),
-        sitename="MixedModelsDatasets.jl",
+         #  format=Documenter.HTML(; size_threshold=500_000, size_threshold_warn=250_000),
+         sitename="MixedModelsDatasets.jl",
          doctest=true,
          # pagesonly=true,
          # warnonly=true,
          # warnonly=[:cross_references],
-         pages=["index.md", 
+         pages=["index.md",
                 "datasets.md"])
 
 deploydocs(; repo="github.com/JuliaMixedModels/MixedModelsDatasets.jl", push_preview=true,
