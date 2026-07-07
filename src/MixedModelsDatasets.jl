6e4416c928c3f88d9d70cce084c5710b02eb6b0f
Phillip Alday <me@phillipalday.com>
2026-07-07 02:31:11 +0000

    YAS
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/MixedModelsDatasets.jl
+++ src/MixedModelsDatasets.jl
@@ -4,14 +4,14 @@ using Arrow
 using DelimitedFiles
 using Downloads
 using ProgressMeter
-using Scratch 
+using Scratch
 using SHA
 
 export dataset, datasets
 
 const CACHE = Ref("")
 const CACHED_DATASETS = Dict{String,String}()
-const DATASETS = Dict{String,NamedTuple}() 
+const DATASETS = Dict{String,NamedTuple}()
 
 """
     __init__()
@@ -23,11 +23,11 @@ function __init__()
     CACHE[] = @get_scratch!("data")
 
     csv = joinpath(@__DIR__, "datasets.csv")
-    data, headers = readdlm(csv, ',', String; 
+    data, headers = readdlm(csv, ',', String;
                             header=true, skipblanks=true, comments=true)
     names = Symbol.(Tuple(headers))
     rows = NamedTuple{names}.(eachrow(data))
-    for row in rows 
+    for row in rows
         DATASETS[row.dsname] = row
     end
 
@@ -82,7 +82,6 @@ function _download(nm::AbstractString; info=true)
     return get!(CACHED_DATASETS, nm) do
         nm in keys(DATASETS) ||
             throw(ArgumentError("Dataset \"$nm\" is not available.\nUse MixedModels.datasets() for available names."))
-        
         path = joinpath(CACHE[], nm * ".arrow")
         ds = DATASETS[nm]
         if !isfile(path) || ds.sha2 != bytes2hex(open(sha2_256, path))
