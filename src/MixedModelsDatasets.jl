19a6d6e93390316950ff5b43be4c7df2fd59e070
Phillip Alday <me@phillipalday.com>
2026-07-06 20:25:21 -0500

    rework to dynamically download and use a scratchspace instead of artifacts

--- src/MixedModelsDatasets.jl
+++ src/MixedModelsDatasets.jl
@@ -1,13 +1,34 @@
 module MixedModelsDatasets
 
 using Arrow
-using Artifacts
-using LazyArtifacts
+using DelimitedFiles
+using Downloads
+using ProgressMeter
+using Scratch 
+using SHA
 
 export dataset, datasets
 
-_testdata() = artifact"TestData"
-cacheddatasets = Dict{String,Arrow.Table}()
+const CACHE = Ref("")
+const CACHED_DATASETS = Dict{String,String}()
+const DATASETS = Dict{String,NamedTuple}() 
+
+function __init__()
+    CACHE[] = @get_scratch!("data")
+
+    csv = joinpath(@__DIR__, "datasets.csv")
+    data, headers = readdlm(csv, ',', String; 
+                            header=true, skipblanks=true, comments=true)
+    names = Symbol.(Tuple(headers))
+    rows = NamedTuple{names}.(eachrow(data))
+    for row in rows 
+        DATASETS[row.dsname] = row
+    end
+
+    return nothing
+end
+
+clear_scratchspaces!() = Scratch.clear_scratchspaces!(@__MODULE__)
 
 """
     dataset(nm)
@@ -18,13 +39,8 @@ Return, as an `Arrow.Table`, the test data set named `nm`, which can be a `Strin
     Dataset names are case insensitive: internally all names are normalized to lowercase.
 """
 function dataset(nm::AbstractString)
-    get!(cacheddatasets, nm) do
-        path = joinpath(_testdata(), lowercase(nm) * ".arrow")
-        if !isfile(path)
-            throw(ArgumentError("Dataset \"$nm\" is not available.\nUse MixedModels.datasets() for available names."))
-        end
-        return Arrow.Table(path)
-    end
+    path = _download(nm)
+    return Arrow.Table(path)
 end
 dataset(nm::Symbol) = dataset(string(nm))
 
@@ -33,9 +49,44 @@ dataset(nm::Symbol) = dataset(string(nm))
 
 Return a vector of names of the available test data sets
 """
-function datasets()
-    return first.(Base.Filesystem.splitext.(filter(endswith(".arrow"),
-                                                   readdir(_testdata()))))
+function datasets(; downloaded=false)
+    ds = collect(keys(DATASETS))
+    if downloaded
+        intersect!(ds, keys(CACHED_DATASETS))
+    end
+    return sort!(ds)
 end
+
+function _download(nm::AbstractString; info=true)
+    return get!(CACHED_DATASETS, nm) do
+        nm in keys(DATASETS) ||
+            throw(ArgumentError("Dataset \"$nm\" is not available.\nUse MixedModels.datasets() for available names."))
+        
+        path = joinpath(CACHE[], lowercase(nm) * ".arrow")
+        ds = DATASETS[nm]
+        if !isfile(path) || ds.sha2 != bytes2hex(open(sha2_256, path))
+            info && @info "Downloading dataset..."
+            url = string("https://osf.io/", ds.filename, "/download?version=", ds.version)
+            Downloads.download(url, path)
+            ds.sha2 == bytes2hex(open(sha2_256, path)) ||
+                error("Downloaded file failed checksum verification.")
+            info && @info "done"
+        end
+
+        return path
+    end
+end
+
+function download_all()
+    @showprogress asyncmap(x -> _download(x; info=false), collect(keys(DATASETS)))
+    return nothing
+end
+
+# function hex(row)
+#        Downloads.download(
+#                           string("https://osf.io/", row.filename, "/download?version=", row.version),
+#                            joinpath(CACHE[], lowercase(row.dsname) * ".arrow"))
+#        bytes2hex(open(sha2_256,joinpath(CACHE[], lowercase(row.dsname) * ".arrow")))
+# end
 
 end # module MixedModelsDatasets
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
 end
 
 @testset "datasets" begin
-    @test length(datasets()) == 17
+    @test length(datasets()) == 26
 
     @testset "$(ds) loadable" for ds in datasets()
         @test dataset(ds) isa Arrow.Table
